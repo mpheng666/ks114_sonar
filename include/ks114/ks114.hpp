acea6ab0883f2f0e8a5b575e32805519f91bd2ed
mpheng666 <mpheng96@gmail.com>
2022-09-06 17:38:30 +0800

    Refactoring

--- include/ks114/ks114.hpp
+++ include/ks114/ks114.hpp
@@ -1,7 +1,7 @@
-#ifndef KS114_SONAR_KS114_DRIVER_HPP
-#define KS114_SONAR_KS114_DRIVER_HPP
+#ifndef _KS114_SONAR_KS114_DRIVER_HPP_
+#define _KS114_SONAR_KS114_DRIVER_HPP_
 
-// #define DEBUG_
+#define DEBUG 
 
 #include <serial/serial.h>
 #include <vector>
@@ -10,6 +10,7 @@
 #include <ros/ros.h>
 #include <std_msgs/Int32MultiArray.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Float64MultiArray.h>
 
 #include <sensor_msgs/Range.h>
 
@@ -26,12 +27,12 @@ namespace ks114_ns
     private:
         // ros
         ros::NodeHandle private_nh_;
-        ros::NodeHandle relative_nh_;
+        // ros::NodeHandle relative_nh_;
         ros::Publisher ks114_sonar_data_pub_;
         ros::Publisher ks114_sonar_auxi_pub_;
-        std_msgs::Float32MultiArray ks114_sonar_raw_msg_;
-        std_msgs::Float32MultiArray ks114_sonar_auxi_msg_;
-        static constexpr double LOOP_RATE {25};
+        // std_msgs::Float32MultiArray ks114_sonar_auxi_msg_;
+        static constexpr double LOOP_RATE {25.0};
+        // ros::Timer timer_send_detection;
 
         // Serial port
         enum class PortState
@@ -41,45 +42,49 @@ namespace ks114_ns
             PortErrored
         };
 
+        enum class DetectionMode
+        {
+            NormalDetection,
+            FastDetection
+        };
+
         PortState port_state_ = PortState::PortUnopened;
-        static constexpr char DEFAULT_PORT[] {"/dev/ttyUSB0"};
-        static constexpr int DEFAULT_BAUDRATE {115200};
         static constexpr uint32_t PORT_TIMEOUT_MS {1000};
-        std::string port_;
-        int baud_rate_;
+        std::string port_ {"/dev/ttyUSB0"};
+        int baud_rate_ {115200};
         serial::Serial ser_;
 
         // Sonar
-        static constexpr uint8_t sensor_address[20] 
+        static constexpr std::array<uint8_t, 20> SENSOR_ADDRESS 
         {
             0XD0, 0XD2, 0XD4, 0XD6, 0XD8, 0XDA, 0XDC, 0XDE, 
             0XE0, 0XE2, 0XE4, 0XE6, 0XE8, 0XEA, 0XEC, 0XEE, 
             0XF8, 0XFA, 0XFC, 0XFE
         };
+        static constexpr uint8_t GET_INFO_COMMAND {0x99};
+        static constexpr uint8_t DETECT_FAST_COMMAND {0x0F};
+        static constexpr uint8_t DETECT_NORMAL_COMMAND {0xB0};
         static constexpr int DEFAULT_NUM {8};
-        int num_of_sonar_;
-        bool check_flag_ {false};
+        static constexpr double RANGE_MIN {0.03};
+        static constexpr double RANGE_MAX_NORMAL {5.95};
+        static constexpr double RANGE_MAX_FAST {1.05};
+        static constexpr double RANGE_ERROR {6.0};
+        int num_of_sonar_ {8};
         std::vector<float> ks114_distance_data_;
-        static constexpr double range_min_ {3.0};
-        static constexpr double range_max_ {105.0};
-        static constexpr double range_error_ {600.0};
-        static constexpr int get_info_command[2] {0x20, 0x99};
-        static constexpr int detect_fast_command[2] {0x20, 0x0F};
-        static constexpr int detect_normal_command[2] {0x20, 0xB0};
+        int detection_mode_param_ {0};
+        DetectionMode detection_mode_ {DetectionMode::NormalDetection};
+        double detection_rate_ {10.0};
 
         // Auxi
-        static constexpr int auxi_size_ {8};
-        static constexpr double auxi_ignored_data_ {5.0};
+        // static constexpr int auxi_size_ {8};
+        // static constexpr double auxi_ignored_data_ {5.0};
 
         void loadParam();
-        bool openSerial(const char* port, const int baudrate);
-        void getSensorInfo(uint8_t);
-        void checkSensorInfo(int);
-        void sendReadCmd(uint8_t);
-        float readSensorValue(int);
-        void pubSensorData();
-        void changeSensorAddress();
-        float filterSensorData(float&);
+        bool openSerial(const std::string port, const int baudrate);
+        void initSensorsInfo();
+        bool readSensorsValue(std::vector<double>& output);
+        double filterSensorsData(const double input, const DetectionMode mode);
+        void pubSensorsData(const std::vector<double>& output);
 
     };
 }; //ks114_ns
--- src/ks114.cpp
+++ src/ks114.cpp
@@ -4,11 +4,12 @@ namespace ks114_ns
 {
 
     SonarKs114::SonarKs114(ros::NodeHandle& nh)
-        : ks114_sonar_data_pub_(nh.advertise<std_msgs::Float32MultiArray>("sonar_data", 10)),
-          ks114_sonar_auxi_pub_(nh.advertise<std_msgs::Float32MultiArray>("sonar_auxi", 10))
+        : 
+        private_nh_(nh),
+        ks114_sonar_data_pub_(nh.advertise<std_msgs::Float32MultiArray>("sonar_data", 10)),
+        ks114_sonar_auxi_pub_(nh.advertise<std_msgs::Float32MultiArray>("sonar_auxi", 10))
+        // timer_send_detection(nh.createTimer(ros::Duration(1.0/detection_rate_), &SonarKs114::readSonar, this))
     {
-        ks114_sonar_raw_msg_.data.resize(num_of_sonar_, 0);
-        ks114_sonar_auxi_msg_.data.resize(auxi_size_, 0);
     }
 
     SonarKs114::SonarKs114()
@@ -21,22 +22,26 @@ namespace ks114_ns
 
     void SonarKs114::loadParam()
     {
-        // Add detect mode param
-        if (!ros::param::param<int>("/ks114_sonar/num_of_sonar", num_of_sonar_, DEFAULT_NUM))
+        if(!private_nh_.getParam("detection_mode", detection_mode_param_))
         {
-            ROS_WARN("Number of sonar is not set. Using %u as default", DEFAULT_NUM);
+            ROS_WARN("Sonar detection mode is not set. Using %u as default", detection_mode_param_);
+            detection_mode_ = static_cast<DetectionMode>(detection_mode_param_);
         }
-        if (!ros::param::param<std::string>("/ks114_sonar/serial_port", port_, DEFAULT_PORT))
+        if (!private_nh_.getParam("num_of_sonar", num_of_sonar_))
         {
-            ROS_WARN("Sonar serial port is not set. Using %s as default", DEFAULT_PORT);
+            ROS_WARN("Number of sonar is not set. Using %u as default", num_of_sonar_);
         }
-        if (!ros::param::param<int>("/ks114_sonar/serial_baud_rate", baud_rate_, DEFAULT_BAUDRATE))
+        if (!private_nh_.getParam("/ks114_sonar/serial_port", port_))
         {
-            ROS_WARN("Sonar baud rate is not set. Using %u as default", DEFAULT_BAUDRATE);
+            ROS_WARN("Sonar serial port is not set. Using %s as default", port_.c_str());
+        }
+        if (!private_nh_.getParam("/ks114_sonar/serial_baud_rate", baud_rate_))
+        {
+            ROS_WARN("Sonar baud rate is not set. Using %u as default", baud_rate_);
         }
     }
 
-    bool SonarKs114::openSerial(const char* port, const int baudrate)
+    bool SonarKs114::openSerial(const std::string port, const int baudrate)
     {
 
         if (port_state_ == PortState::PortUnopened)
@@ -49,9 +54,9 @@ namespace ks114_ns
                 ser_.setTimeout(timeout);
                 ser_.open();
             }
-            catch (serial::IOException &e)
+            catch (const std::exception &exc)
             {
-                ROS_ERROR_STREAM("Unable to open port ");
+                ROS_ERROR_STREAM("Unable to open port!" << exc.what());
                 port_state_ = PortState::PortErrored;
                 return false;
             }
@@ -62,139 +67,174 @@ namespace ks114_ns
                 port_state_ = PortState::PortOpened;
                 return true;
             }
-            else
-            {
-                port_state_ = PortState::PortUnopened;
-                return false;
-            }
         }
+
+        return false;
     }
 
     void SonarKs114::startProcess()
     {
         this->loadParam();
         ros::Rate r(LOOP_RATE);
-        ros::Rate r2(5);
-        uint32_t distance_;
+
+        if(this->openSerial(port_.c_str(), baud_rate_))
+        {
+            this->initSensorsInfo();
+        }
 
         while (ros::ok())
         {
-            if (port_state_ == PortState::PortOpened && check_flag_ == true)
+            if (port_state_ == PortState::PortOpened)
             {
-                for (int i = 0; i < num_of_sonar_; i++)
+                std::vector<double> sensors_output;
+                if(this->readSensorsValue(sensors_output))
                 {
-                    this->sendReadCmd(sensor_address[i]);
-                    r.sleep();
-                    ks114_distance_data_.push_back(this->readSensorValue(i));
+                    #ifdef DEBUG
+                        for(const auto& o : sensors_output)
+                        {
+                            ROS_INFO_STREAM("output: " << o << " ");
+                        }
+                    #endif
+                    this->pubSensorsData(sensors_output);
                 }
-                this->pubSensorData();
-                ks114_distance_data_.clear();
             } 
-            else if (port_state_ == PortState::PortUnopened)
+            else if (port_state_ == PortState::PortUnopened || port_state_ == PortState::PortErrored)
             {
                 this->openSerial(port_.c_str(), baud_rate_);
             }
-            else if (port_state_ == PortState::PortOpened && check_flag_ == false)
-            {
-                for (int i = 0; i < num_of_sonar_; i++)
-                {
-                    this->getSensorInfo(sensor_address[i]);
-                    r.sleep();
-                    this->checkSensorInfo(i);
-                }
-                check_flag_ = true;
-            }
+            r.sleep();
             ros::spinOnce();
         }
     }
 
-    void SonarKs114::getSensorInfo(uint8_t address_)
+    void SonarKs114::initSensorsInfo()
     {
-        uint8_t info_cmd_single[3] = {address_, 0x02, 0x99};
-        ser_.flushInput();
-        ser_.write(info_cmd_single, 3);
-    }
+        int index = 0;
+        std::for_each(
+            SENSOR_ADDRESS.begin(), 
+            SENSOR_ADDRESS.begin()+num_of_sonar_,
+            [&](auto& address)
+            {
+                ser_.flushInput();
+                const uint8_t command[3] {address, 0x02, GET_INFO_COMMAND};
+                ser_.write(command, 3);
 
-    void SonarKs114::checkSensorInfo(int index)
-    {
-        std::vector<uint8_t> byte_received;
+                ros::Rate(LOOP_RATE).sleep();
 
-        if (ser_.available())
-        {
-            ser_.read(byte_received, 22);
-            if(byte_received[5] == sensor_address[index])
-            ROS_INFO("Sonar %d is CONNECTED!", index);
-        }
-        else
-        {
-            ROS_ERROR("Sonar %d is NOT CONNECTED OR CONFIGURED!", index);
-        }
-        byte_received.clear();
-    }
+                std::vector<uint8_t> byte_received;
 
-    void SonarKs114::sendReadCmd(uint8_t address_)
-    {
-        // B0 for high speed read (0.01m ~ 1.1m)
-        uint8_t detect_cmd_single[3] = {address_, 0x02, 0x0F}; 
-        // 0F for normal speed read (0.03m ~ 5.6m)
-        // uint8_t detect_cmd_single[3] = {address_, 0x02, 0xB0}; 
-        ser_.flushInput();
-        ser_.write(detect_cmd_single, 3);
-    }
-
-    float SonarKs114::readSensorValue(int index)
-    {
-        std::vector<uint8_t> byte_received;
-        uint16_t distance_uint16_raw_ = 0;
-        float distance_raw_ = 0.0;
-        float distance_filtered_ = 0.0;
-
-        if (ser_.available())
-        {
-            ser_.read(byte_received, 2);
-            distance_uint16_raw_ = (uint16_t(byte_received[0] << 8 | byte_received[1]));
-            distance_raw_ = float(distance_uint16_raw_)/58.0;
-
-            #ifdef DEBUG_
-                if (index == 0)
+                if(ser_.available())
                 {
-                    ROS_INFO("serial read: %02X %02X", byte_received[0], byte_received[1]);
-                    ROS_INFO("distance %d : %f \n", index, distance_raw_);
+                    ser_.read(byte_received, 22);
+
+                    #ifdef DEBUG
+                        for(const auto& b : byte_received)
+                        {
+                            ROS_INFO("b: %02X", b);
+                        }
+                    #endif
+   
+                    if(byte_received.at(5) == address)
+                    {
+                        ROS_INFO("Sonar %d is CONNECTED!", index);
+                    }
                 }
-            #endif
-            
+                else
+                {
+                    ROS_ERROR("Sonar %d is NOT CONNECTED OR CONFIGURED!", index);
+                }
+                ++index;
+                byte_received.clear();
+            }
+        );
+    }
+
+    bool SonarKs114::readSensorsValue(std::vector<double>& output)
+    {
+        int index = 0;
+        std::for_each(
+            SENSOR_ADDRESS.begin(), 
+            SENSOR_ADDRESS.begin()+num_of_sonar_,
+            [&](auto& address)
+            {
+                ser_.flushInput();
+                const uint8_t command[3] {address, 0x02, DETECT_FAST_COMMAND};
+                ser_.write(command, 3);
+
+                ros::Rate(LOOP_RATE).sleep();
+
+                std::vector<uint8_t> byte_received;
+
+                if(ser_.available())
+                {
+                    ser_.read(byte_received, 2);
+                    auto value_in_byte = static_cast<uint16_t>(byte_received[0] << 8 | byte_received[1]);
+                    auto value_in_m = static_cast<float>(value_in_byte)/5800.0;
+                    output.push_back(this->filterSensorsData(value_in_m, detection_mode_));
+
+                    #ifdef DEBUG
+                        for(const auto& b : byte_received)
+                        {
+                            ROS_INFO("b: %02X", b);
+                        }
+                    #endif
+   
+                }
+                else
+                {
+                    ROS_ERROR("Sonar %d is NOT CONNECTED!", index);
+                }
+                ++index;
+                byte_received.clear();
+            }
+        );
+        if(output.size() > 0)
+        {
+            return true;
         }
         else
         {
-            ROS_ERROR("Sonar %d is NOT CONNECTED!", index);
+            return false;
         }
-        byte_received.clear();
-        distance_filtered_ = filterSensorData(distance_raw_);
-
-        return (distance_filtered_/100.0);
     }
 
-    float SonarKs114::filterSensorData(float &distance_unfiltered_)
+    double SonarKs114::filterSensorsData(const double input, const DetectionMode mode)
     {
-        if(distance_unfiltered_ > this->range_min_ && distance_unfiltered_ < this->range_max_)
-            return distance_unfiltered_;
-        else 
-            return this->range_error_;
+        if(mode == DetectionMode::FastDetection)
+        {
+            if(input > RANGE_MIN && input < RANGE_MAX_FAST)
+                return input;
+            else 
+                return RANGE_ERROR;
+        }
+        else if (mode == DetectionMode::NormalDetection)
+        {
+            if(input > RANGE_MIN && input < RANGE_MAX_NORMAL)
+                return input;
+            else 
+                return RANGE_ERROR;
+        }
     }
 
-    void SonarKs114::pubSensorData()
+    void SonarKs114::pubSensorsData(const std::vector<double>& output)
     {
-        std::copy(ks114_distance_data_.begin(), ks114_distance_data_.end(), ks114_sonar_raw_msg_.data.begin());
-        ks114_sonar_auxi_msg_.data[0] = ks114_distance_data_[0];
-        ks114_sonar_auxi_msg_.data[1] = ks114_distance_data_[1];
-        ks114_sonar_auxi_msg_.data[2] = ks114_distance_data_[2];
-        ks114_sonar_auxi_msg_.data[3] = ks114_distance_data_[3];
-        ks114_sonar_auxi_msg_.data[4] = ks114_distance_data_[4];
-        ks114_sonar_auxi_msg_.data[5] = ks114_distance_data_[5];
-        ks114_sonar_auxi_msg_.data[6] = ks114_distance_data_[6];
-        ks114_sonar_auxi_msg_.data[7] = ks114_distance_data_[7];
-        ks114_sonar_raw_pub_.publish(ks114_sonar_raw_msg_);
-        ks114_sonar_auxi_pub_.publish(ks114_sonar_auxi_msg_);
+        // assert(output.size() == num_of_sonar_);
+        std_msgs::Float64MultiArray sonar_msg;
+        sonar_msg.data.resize(num_of_sonar_, 0);
+        // std::copy(ks114_distance_data_.begin(), ks114_distance_data_.end(), ks114_sonar_raw_msg_.data.begin());
+        // sonar_msg.data = std::move(output);
+        std::copy(output.begin(), output.end(), sonar_msg.data.begin());
+        ks114_sonar_data_pub_.publish(sonar_msg);
+
+        // ks114_sonar_auxi_msg_.data[0] = ks114_distance_data_[0];
+        // ks114_sonar_auxi_msg_.data[1] = ks114_distance_data_[1];
+        // ks114_sonar_auxi_msg_.data[2] = ks114_distance_data_[2];
+        // ks114_sonar_auxi_msg_.data[3] = ks114_distance_data_[3];
+        // ks114_sonar_auxi_msg_.data[4] = ks114_distance_data_[4];
+        // ks114_sonar_auxi_msg_.data[5] = ks114_distance_data_[5];
+        // ks114_sonar_auxi_msg_.data[6] = ks114_distance_data_[6];
+        // ks114_sonar_auxi_msg_.data[7] = ks114_distance_data_[7];
+        // ks114_sonar_auxi_pub_.publish(ks114_sonar_auxi_msg_);
     }
 
 }; // ks114_ns
