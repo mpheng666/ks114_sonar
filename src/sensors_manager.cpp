30187d56045c4a8e133209a3025f420f7a22af34
mpheng666 <mpheng96@gmail.com>
2022-10-05 13:38:14 +0800

    Add remapper param checker

--- src/sensors_manager.cpp
+++ src/sensors_manager.cpp
@@ -5,9 +5,12 @@ using namespace sensors_manager;
 SensorsManager::SensorsManager(ros::NodeHandle &nh)
     : nh_p_(nh),
       sonars_pub_(nh_p_.advertise<std_msgs::Float64>("sonars_data", 10)),
-      get_data_stimer_(nh_p_.createSteadyTimer(ros::WallDuration(0.05), &SensorsManager::timerGetDataSteadyCallBack, this)),
+      get_data_stimer_(nh_p_.createSteadyTimer(
+              ros::WallDuration(0.05),
+              &SensorsManager::timerGetDataSteadyCallBack,
+              this)),
       pub_timer_(nh_p_.createTimer(
-          ros::Duration(0.05), &SensorsManager::timerPubCallBack, this))
+              ros::Duration(0.05), &SensorsManager::timerPubCallBack, this))
 
 {
 }
@@ -17,11 +20,11 @@ SensorsManager::~SensorsManager() {}
 void SensorsManager::start()
 {
     loadParams();
+    updateVariables();
     startSensors();
     initRosPub();
     ros::Rate rate = LOOP_RATE_;
-    while (ros::ok())
-    {
+    while (ros::ok()) {
         ros::spinOnce();
         rate.sleep();
     }
@@ -29,35 +32,52 @@ void SensorsManager::start()
 
 void SensorsManager::loadParams()
 {
-    if (!nh_p_.param("detection_mode", detection_mode_, detection_mode_))
-    {
+    if (!nh_p_.param("detection_mode", detection_mode_, detection_mode_)) {
         ROS_WARN_STREAM("detection_mode is not set! Use default "
                         << detection_mode_);
     }
-
-    if (!nh_p_.param("num_of_sensor", num_of_sensor_, num_of_sensor_))
-    {
+    if (!nh_p_.param("num_of_sensor", num_of_sensor_, num_of_sensor_)) {
         ROS_WARN_STREAM("num_of_sensor is not set! Use default "
                         << num_of_sensor_);
     }
-
-    if (!nh_p_.param("serial_port", serial_port_, serial_port_))
-    {
+    if (!nh_p_.param("serial_port", serial_port_, serial_port_)) {
         ROS_WARN_STREAM("serial_port is not set! Use default " << serial_port_);
     }
     if (!nh_p_.param("serial_baud_rate", serial_baud_rate_,
-                     serial_baud_rate_))
-    {
+                     serial_baud_rate_)) {
         ROS_WARN_STREAM("serial_baud_rate is not set! Use default "
                         << serial_baud_rate_);
     }
-    if (!nh_p_.param("sonar_remapper", sonar_remapper_,
-                     sonar_remapper_))
-    {
-        for (const auto &remapper : sonar_remapper_)
-        {
-            ROS_WARN_STREAM("sonar_remapper is not set! Use default "
-                            << remapper);
+    if (!nh_p_.param("sonar_remapper", sonar_remapper_, sonar_remapper_)) {
+        ROS_WARN_STREAM(
+                "sonar_remapper is not set! Use default: \n1 2 3 4 5 6 7 8");
+    }
+    else {
+        std::vector<int> sonar_remapper_sorted(sonar_remapper_.size());
+        std::partial_sort_copy(sonar_remapper_.begin(), sonar_remapper_.end(),
+                               sonar_remapper_sorted.begin(),
+                               sonar_remapper_sorted.end());
+        auto checkElementsOutOfRange = [](const int &n) {
+            return (n < 1 || n > 20) ? true : false;
+        };
+        if (sonar_remapper_.size() != 8) {
+            sonar_remapper_ = {1, 2, 3, 4, 5, 6, 7, 8};
+            ROS_WARN_STREAM("sonar_remapper is wrong-sized! Use default: \n1 2 "
+                            "3 4 5 6 7 8");
+        }
+        else if (std::any_of(sonar_remapper_.begin(), sonar_remapper_.end(),
+                             checkElementsOutOfRange)) {
+            sonar_remapper_ = {1, 2, 3, 4, 5, 6, 7, 8};
+            ROS_WARN_STREAM(
+                    "sonar_remapper is out-of-ranged! Use default: \n1 2 "
+                    "3 4 5 6 7 8");
+        }
+        else if (std::unique(sonar_remapper_sorted.begin(),
+                             sonar_remapper_sorted.end()) !=
+                 sonar_remapper_sorted.end()) {
+            sonar_remapper_ = {1, 2, 3, 4, 5, 6, 7, 8};
+            ROS_WARN_STREAM("sonar_remapper is duplicated! Use default: \n1 2 "
+                            "3 4 5 6 7 8");
         }
     }
 }
@@ -65,28 +85,31 @@ void SensorsManager::loadParams()
 void SensorsManager::updateVariables()
 {
     ks114_detection_mode_ =
-        (detection_mode_ == 0)
-            ? ks114_sonar::DetectionMode::Far
-        : (detection_mode_ == 1) ? ks114_sonar::DetectionMode::Fast
-                                 : ks114_sonar::DetectionMode::Fast;
+            (detection_mode_ == 0)
+                    ? ks114_sonar::DetectionMode::Far
+                    : (detection_mode_ == 1) ? ks114_sonar::DetectionMode::Fast
+                                             : ks114_sonar::DetectionMode::Fast;
     data_msgs_.data.resize(num_of_sensor_);
     range_msgs_.resize(num_of_sensor_);
-    sonar_remapper_.resize(num_of_sensor_);
+    for (const auto &pos : ROBOT_BODY | boost::adaptors::indexed(0)) {
+        sonar_position_map_.emplace(pos.value(),
+                                    sonar_remapper_.at(pos.index()));
+    }
+    // for (const auto &[key, value] : sonar_position_map_) {
+    //     std::cout << key << " : " << value << "\n";
+    // }
 }
 
 void SensorsManager::startSensors()
 {
-    for (const auto &sonar : sonars_ | boost::adaptors::indexed(0))
-    {
+    for (const auto &sonar : sonars_ | boost::adaptors::indexed(0)) {
         sonar.value().setIndex(sonar.index() + 1);
         sonar.value().setSerialPort(serial_port_);
         sonar.value().setSerialBaudRate(serial_baud_rate_);
     }
     std::for_each(sonars_.begin(), (sonars_.begin() + num_of_sensor_),
-                  [](auto &sonar)
-                  {
-                      if (sonar.start())
-                      {
+                  [](auto &sonar) {
+                      if (sonar.start()) {
                           sonar.getSonarConfig().printConfig();
                       }
                   });
@@ -96,29 +119,26 @@ void SensorsManager::initRosPub()
 {
     range_sensors_pubs_.resize(num_of_sensor_);
     for (const auto &range_sensor_pub :
-         range_sensors_pubs_ | boost::adaptors::indexed(0))
-    {
+         range_sensors_pubs_ | boost::adaptors::indexed(0)) {
         std::string topic_name = "sonar_range_" + range_sensor_pub.index();
         range_sensor_pub.value() =
-            nh_p_.advertise<sensor_msgs::Range>(topic_name, 10);
+                nh_p_.advertise<sensor_msgs::Range>(topic_name, 10);
     }
 }
 
 void SensorsManager::timerGetDataSteadyCallBack(const ros::SteadyTimerEvent &)
 {
-    std::for_each(sonars_.begin(), (sonars_.begin() + num_of_sensor_),
-                  [&](auto &sonar)
-                  {
-                      sonar.getDistance(ks114_detection_mode_, // TODO );
-                  });
+    // std::for_each(sonars_.begin(), (sonars_.begin() + num_of_sensor_),
+    //               [&](auto &sonar) {
+    //                   sonar.getDistance(ks114_detection_mode_, // TODO );
+    //               });
 }
 
 void SensorsManager::timerPubCallBack(const ros::TimerEvent &)
 {
     std_msgs::Float64 msg;
     std::for_each(sonars_.begin(), (sonars_.begin() + num_of_sensor_),
-                  [&](auto &sonar)
-                  {
+                  [&](auto &sonar) {
                       sonar.getDistance(ks114_detection_mode_, msg.data);
                       sonars_pub_.publish(msg);
                   });
@@ -128,14 +148,13 @@ void SensorsManager::timerPubCallBack(const ros::TimerEvent &)
     range_msg.header.stamp = ros::Time::now();
     range_msg.radiation_type = range_msg.ULTRASOUND;
     range_msg.min_range =
-        (ks114_detection_mode_ == ks114_sonar::DetectionMode::Far) ? 0.03
-                                                                   : 0.01;
+            (ks114_detection_mode_ == ks114_sonar::DetectionMode::Far) ? 0.03
+                                                                       : 0.01;
     range_msg.max_range =
-        (ks114_detection_mode_ == ks114_sonar::DetectionMode::Far) ? 1.1
-                                                                   : 5.6;
+            (ks114_detection_mode_ == ks114_sonar::DetectionMode::Far) ? 1.1
+                                                                       : 5.6;
     for (const auto &range_sensor_pub :
-         range_sensors_pubs_ | boost::adaptors::indexed(0))
-    {
+         range_sensors_pubs_ | boost::adaptors::indexed(0)) {
         range_sensor_pub.value().publish(range_msg);
     }
 }
