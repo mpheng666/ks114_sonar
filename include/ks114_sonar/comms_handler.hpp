b032b9579a4c31fd41a77918790cea711af94f95
mpheng666 <mpheng96@gmail.com>
2023-03-27 23:45:47 +0800

    Add comms handler

---
+++ include/ks114_sonar/comms_handler.hpp
@@ -0,0 +1,19 @@
+#ifndef KS114_SONAR_COMMS_HANDLER_HPP_
+#define KS114_SONAR_COMMS_HANDLER_HPP_
+
+namespace comms_handler
+{
+    enum class CommState : int{
+        OPENED,
+        CLOSED,
+        ERRORED
+    };
+
+    class CommsHandler
+    {
+
+    };
+
+}
+
+#endif
